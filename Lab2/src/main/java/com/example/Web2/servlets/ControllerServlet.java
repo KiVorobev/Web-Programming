4e542ce62b1754ef47f911b74e2ec44286e0eedb
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-10-28 00:08:06 +0300

    Create ControllerServlet.java

---
+++ Lab2/src/main/java/com/example/Web2/servlets/ControllerServlet.java
@@ -0,0 +1,22 @@
+package com.example.Web2.servlets;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+public class ControllerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+            String x = request.getParameter("X");
+            String y = request.getParameter("Y");
+            String r = request.getParameter("R");
+
+            if ((x.trim() != null && y.trim() != null && r.trim() != null) && (x.trim() != "" && y.trim() != "" && r.trim() != "")) {
+                getServletContext().getNamedDispatcher("AreaCheckServlet").forward(request, response);
+            } else {
+                getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+            }
+    }
+}
