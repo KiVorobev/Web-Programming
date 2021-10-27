c5febf1103b12b62959dad3b7ce8d7ef33eb1240
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-10-28 00:07:56 +0300

    Create Data.java

---
+++ Lab2/src/main/java/com/example/Web2/results/Data.java
@@ -0,0 +1,70 @@
+package com.example.Web2.results;
+
+public class Data {
+    private int x;
+    private double y;
+    private double r;
+    private String currentTime;
+    private long executionTime;
+    private boolean result;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    public String getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(String currentTime) {
+        this.currentTime = currentTime;
+    }
+
+    public boolean isResult() {
+        return result;
+    }
+
+    public void setResult(boolean result) {
+        this.result = result;
+    }
+
+    public long getExecutionTime() {
+        return executionTime;
+    }
+
+    public void setExecutionTime(long executionTime) {
+        this.executionTime = executionTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "x=" + x +
+                ", y=" + y +
+                ", r=" + r +
+                ", currentTime='" + currentTime + '\'' +
+                ", executionTime=" + executionTime +
+                ", result=" + result +
+                '}';
+    }
+}
