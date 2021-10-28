ddd26761a78fdd2aebaac516d5d3f5e07fedd732
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-10-28 08:09:54 +0300

    Create click.js

---
+++ Lab2/src/main/webapp/js/click.js
@@ -0,0 +1,48 @@
+function checkClick(event) {
+    let r = $('#R').val();
+    let rError = document.getElementById('R_error');
+
+    if (checkR(rError)) {
+        let coordinateX = event.pageX - 926;
+        let coordinateY = event.pageY - 180;
+        let x = r * (coordinateX - 175) / 135 ;
+        let y = r * (175 - coordinateY) / 135;
+        sendRequest(x, y, r);
+    }
+}
+
+function checkR(rError) {
+    let allRightR = true;
+    if (r.value.trim() != null && r.value.trim() != "") {
+        if (/^(0$|-?[1-9]\d*(\.\d*[1-9]$)?|-?0\.\d*[1-9])$/.test(r.value)) {
+            if ((parseInt(r.value) > 1) && (parseInt(r.value) < 5)) {
+                rError.innerHTML = "";
+                allRightR = true;
+            } else {
+                rError.innerHTML = "R can take value in the range (2 ... 5)!";
+                allRightR = false;
+            }
+        } else {
+            rError.innerHTML = "Value must be a number!";
+            allRightR = false;
+        }
+    } else {
+        rError.innerHTML = "Value cannot be empty!";
+        allRightR = false;
+    }
+    return allRightR;
+}
+
+function sendRequest(x, y, r) {
+   $.ajax({
+       url: "./processing",
+       type: "GET",
+       data: {'X': x, 'Y': y, 'R': r},
+       success: function () {
+           window.location.reload(true);
+       },
+       error: function () {
+           alert(this.error);
+       }
+   });
+}
