55dae55e460a5cabea72cdef8dc30aa9bd057035
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-10-29 02:38:24 +0300

    Update click.js

--- Lab2/src/main/webapp/js/click.js
+++ Lab2/src/main/webapp/js/click.js
@@ -1,9 +1,13 @@
 function checkClick(event) {
     let r = $('#R').val();
     let rError = document.getElementById('R_error');
+    let yError = document.getElementById('Y_error');
+    let xError = document.getElementById('X_error');
 
+    yError.innerHTML = "";
+    xError.innerHTML = "";
     if (checkR(rError)) {
-        let coordinateX = event.pageX - 926;
+        let coordinateX = event.pageX - 925;
         let coordinateY = event.pageY - 180;
         let x = r * (coordinateX - 175) / 135 ;
         let y = r * (175 - coordinateY) / 135;
@@ -41,5 +45,10 @@ function sendRequest(x, y, r) {
        success: function () {
            window.location.reload(true);
        },
+       error: function (response) {
+           if (x < -3 || x > 5 || y < -5 || y > 3) {
+               alert("Invalid x and y values. Please choose valid values.");
+           }
+       }
    });
 }
