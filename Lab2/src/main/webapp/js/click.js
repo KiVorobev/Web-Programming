3eb7c9446819edf07e64fd15a5b4eadfd7353afa
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-11-05 20:49:22 +0300

    Update click.js

--- Lab2/src/main/webapp/js/click.js
+++ Lab2/src/main/webapp/js/click.js
@@ -3,15 +3,18 @@ function checkClick(event) {
     let rError = document.getElementById('R_error');
     let yError = document.getElementById('Y_error');
     let xError = document.getElementById('X_error');
+    let pictureError = document.getElementById('picture_error');
 
     yError.innerHTML = "";
     xError.innerHTML = "";
     if (checkR(rError)) {
         let coordinateX = event.pageX - 925;
         let coordinateY = event.pageY - 180;
-        let x = r * (coordinateX - 175) / 135 ;
+        let x = r * (coordinateX - 175) / 135;
         let y = r * (175 - coordinateY) / 135;
-        sendRequest(x, y, r);
+        if (checkXY(x, y, pictureError)) {
+            sendRequest(x, y, r);
+        }
     }
 }
 
@@ -37,18 +40,20 @@ function checkR(rError) {
     return allRightR;
 }
 
+function checkXY(x, y, pictureError) {
+    if (x < -3 || x > 5 || y < -5 || y > 3) {
+        pictureError.innerHTML = "Invalid x and y values. Please choose valid values on the picture.";
+        return false;
+    } else {
+        pictureError.innerHTML = "";
+        return true;
+    }
+}
+
 function sendRequest(x, y, r) {
-   $.ajax({
-       url: "./processing",
-       type: "GET",
-       data: {'X': x, 'Y': y, 'R': r},
-       success: function () {
-           window.location.reload(true);
-       },
-       error: function (response) {
-           if (x < -3 || x > 5 || y < -5 || y > 3) {
-               alert("Invalid x and y values. Please choose valid values.");
-           }
-       }
-   });
+    $.ajax({
+        url: "./processing",
+        type: "GET",
+        data: {'X': x, 'Y': y, 'R': r},
+    });
 }
