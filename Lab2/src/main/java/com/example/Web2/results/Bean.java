074b6752a8a8ec43a124d2b4da49b1e86178367c
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-11-06 20:42:27 +0300

    Update Bean.java

--- Lab2/src/main/java/com/example/Web2/results/Bean.java
+++ Lab2/src/main/java/com/example/Web2/results/Bean.java
@@ -1,5 +1,10 @@
 package com.example.Web2.results;
 
+import org.kopitubruk.util.json.JSONUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bean {
     private double x;
     private double y;
@@ -7,6 +12,26 @@ public class Bean {
     private String currentTime;
     private long executionTime;
     private boolean result;
+
+    public String jsonBean() {
+        System.out.println(this.getBean());
+        System.out.println(JSONUtil.toJSON(this.getBean()));
+        System.out.println("Next");
+        return "a";
+        //return JSONUtil.toJSON(this.getBean());
+    }
+
+    private Map<String, String> getBean() {
+        Map<String, String> bean = new HashMap<>();
+        bean.put("x", String.valueOf(x));
+        bean.put("y", String.valueOf(y));
+        bean.put("r", String.valueOf(r));
+        bean.put("currentTime", String.valueOf(currentTime));
+        bean.put("executionTime", String.valueOf(executionTime));
+        bean.put("result", String.valueOf(result));
+        return bean;
+    }
+
 
     public double getX() {
         return x;
