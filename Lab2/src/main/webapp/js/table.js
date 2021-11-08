7d087971e95c7ea1dff4a5b6c4cd8d31cb9ecc58
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-11-08 17:05:50 +0300

    Update table.js

--- Lab2/src/main/webapp/js/table.js
+++ Lab2/src/main/webapp/js/table.js
@@ -1,13 +1,13 @@
 addBean = (x, y, r, currentTime, executionTime, result) => {
     let newRow = "<tr>";
-    newRow += "<td>" + parseFloat(x).toFixed(2) + "</td>";
-    newRow += "<td>" + parseFloat(y).toFixed(2) + "</td>";
-    newRow += "<td>" + parseFloat(r).toFixed(2) + "</td>";
+    newRow += "<td style=\"max-width: 150px; word-wrap: break-word\";>" + parseFloat(x) + "</td>";
+    newRow += "<td style=\"max-width: 150px; word-wrap: break-word\";>" + parseFloat(y) + "</td>";
+    newRow += "<td style=\"max-width: 150px; word-wrap: break-word\";>" + parseFloat(r)+ "</td>";
     newRow += "<td>" + currentTime + "</td>";
     newRow += "<td>" + executionTime + "</td>";
     newRow += (result === "false" || result === undefined)
-        ? "<td><span style='color: red'>FALSE</span></td>"
-        : "<td><span style='color: green'>TRUE</span></td>";
+        ? "<td><span style='color: red'>false</span></td>"
+        : "<td><span style='color: #0fc40f'>true</span></td>";
     newRow += "</tr>";
     $('#results tr:last').after(newRow);
 }
