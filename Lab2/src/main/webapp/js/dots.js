8e7adf7e1c07acd026b55138c7696b5a9a0dced3
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-11-08 17:05:43 +0300

    Update dots.js

--- Lab2/src/main/webapp/js/dots.js
+++ Lab2/src/main/webapp/js/dots.js
@@ -1,11 +1,11 @@
 function drawDots(x, y, r, result) {
     let svg = document.querySelector('#picture');
-    let circ = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+    let circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
     (result === "true")
-        ? circ.style.fill = "red"
-        : circ.style.fill = "#302929"
-    circ.style.r = "5";
-    circ.style.cx = (x * 135 / r) + 175;
-    circ.style.cy = -(y * 135 / r) + 175;
-    svg.appendChild(circ);
+        ? circle.style.fill = "red"
+        : circle.style.fill = "#302929"
+    circle.style.r = "5";
+    circle.style.cx = (x * 135 / r) + 175;
+    circle.style.cy = -(y * 135 / r) + 175;
+    svg.appendChild(circle);
 }
