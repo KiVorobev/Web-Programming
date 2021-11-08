a0c6f8e6f2c1c27cf355b0e7eb728c2224b73b59
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-11-08 17:38:37 +0300

    Update table.js

--- Lab2/src/main/webapp/js/table.js
+++ Lab2/src/main/webapp/js/table.js
@@ -1,10 +1,10 @@
-addBean = (x, y, r, currentTime, executionTime, result) => {
+function addBean (x, y, r, currentTime, executionTime, result) {
     let newRow = "<tr>";
-    newRow += "<td style=\"max-width: 150px; word-wrap: break-word\";>" + parseFloat(x) + "</td>";
-    newRow += "<td style=\"max-width: 150px; word-wrap: break-word\";>" + parseFloat(y) + "</td>";
-    newRow += "<td style=\"max-width: 150px; word-wrap: break-word\";>" + parseFloat(r)+ "</td>";
+    newRow += "<td style=\"max-width: 150px; word-wrap: break-word\";>" + x + "</td>";
+    newRow += "<td style=\"max-width: 150px; word-wrap: break-word\";>" + y + "</td>";
+    newRow += "<td style=\"max-width: 150px; word-wrap: break-word\";>" + r+ "</td>";
     newRow += "<td>" + currentTime + "</td>";
-    newRow += "<td>" + executionTime + "ms</td>";
+    newRow += "<td>" + executionTime + " ms</td>";
     newRow += (result === "false" || result === undefined)
         ? "<td><span style='color: red'>false</span></td>"
         : "<td><span style='color: #0fc40f'>true</span></td>";
