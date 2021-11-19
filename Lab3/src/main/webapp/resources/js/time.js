c4ac88bbf5b2c097db9cd57288d706d52fdaf371
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-11-19 18:19:42 +0300

    Create time.js

---
+++ Lab3/src/main/webapp/resources/js/time.js
@@ -0,0 +1,10 @@
+update();
+setInterval(update, 9000);
+
+function update() {
+    let date = new Date(),
+        hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
+        minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
+        seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+    document.getElementById('clock').innerHTML = hours + ':' + minutes + ':' + seconds;
+}
