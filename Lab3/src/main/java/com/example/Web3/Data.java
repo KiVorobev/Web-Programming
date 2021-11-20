1c5423bf22beb21101c78bdeda4383fe29be9b28
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-11-20 19:17:51 +0300

    Create Data.java

---
+++ Lab3/src/main/java/com/example/Web3/Data.java
@@ -0,0 +1,11 @@
+package com.example.Web3;
+
+@lombok.Data
+public class Data {
+    private double x;
+    private double y;
+    private double r;
+    private String currentTime;
+    private long executionTime;
+    private boolean result;
+}
