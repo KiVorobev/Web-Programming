816ed1a1f19c7f2ad3a25ce999372ea325899d8e
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-11-25 15:54:01 +0300

    Update time.js

--- Lab3/src/main/webapp/resources/js/time.js
+++ Lab3/src/main/webapp/resources/js/time.js
@@ -3,8 +3,11 @@ setInterval(update, 9000);
 
 function update() {
     let date = new Date(),
+        months = ["January","February","March","April","May","June","July","August","September","October","November","December"],
+        days = (date.getDate() < 10) ? '0' + date.getDate() : date.getDate(),
         hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
         minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
         seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
     document.getElementById('clock').innerHTML = hours + ':' + minutes + ':' + seconds;
+    document.getElementById('date').innerHTML = days + ' ' + months[date.getMonth()] + ' ' + date.getFullYear();
 }
