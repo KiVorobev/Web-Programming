dcc494e1ceb639214f91e8ee597e6fe2ff8a45d5
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-11-25 15:53:59 +0300

    Create click.js

---
+++ Lab3/src/main/webapp/resources/js/click.js
@@ -0,0 +1,8 @@
+function checkHit() {
+    let coordinateX = event.pageX - 910;
+    let coordinateY = event.pageY - 177;
+    let xHidden = document.forms[0].elements[1].value = coordinateX;
+    let yHidden = document.forms[0].elements[2].value = coordinateY;
+
+    document.forms[0].elements[10].click();
+}
