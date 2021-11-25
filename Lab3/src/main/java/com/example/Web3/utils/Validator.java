2cd032db9da8430ea953a70b4489eef42a5bd1b6
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-11-25 15:53:34 +0300

    Update Validator.java

--- Lab3/src/main/java/com/example/Web3/utils/Validator.java
+++ Lab3/src/main/java/com/example/Web3/utils/Validator.java
@@ -1,20 +1,40 @@
 package com.example.Web3.utils;
 
+import com.example.Web3.entity.Result;
+
 public class Validator {
 
-    public boolean validate(Double x, Double y, Integer r){
-        return validateX(x) && validateY(y) && validateR(r);
+    public boolean validate(Result result){
+        return validateX(result) && validateY(result) && validateR(result);
     }
 
-    private boolean validateX(Double x) {
-        return x >= -4 && x <= 4;
+    private boolean validateX(Result result) {
+        return result.getX() >= -4 && result.getX() <= 4 && result.getX() != null;
     }
 
-    private boolean validateY(Double y) {
-        return y > -3 && y < 3;
+    private boolean validateY(Result result) {
+        return result.getY() > -3 && result.getY() < 3 && result.getY() != null;
     }
 
-    private boolean validateR(Integer r) {
-        return r >= 1 && r <= 5;
+    private boolean validateR(Result result) {
+        return result.getR() >= 1 && result.getR() <= 5 && result.getR() != null;
+    }
+
+    public String checkX(Result result) {
+        if (result.getX() == null) return "You need to choose the value X!";
+        else return "";
+    }
+
+    public String checkY(Result result) {
+        if (result.getY() == null) {
+            return "You need to choose the value Y!";
+        } else if(result.getY() <= -3 || result.getY() >= 3) {
+            return "Y can take value in the range (-3 ... 3)!";
+        } else return "";
+    }
+
+    public String checkR(Result result) {
+        if (result.getR() == null) return "You need to choose the value R!";
+        else return "";
     }
 }
