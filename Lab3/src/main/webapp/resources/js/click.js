3310390fd4ebb71eb86b2c1166b0c6f00d2cf7de
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-12-08 23:16:10 +0300

    result color and coordinate edit

--- Lab3/src/main/webapp/resources/js/click.js
+++ Lab3/src/main/webapp/resources/js/click.js
@@ -1,6 +1,6 @@
 function checkHit() {
     let coordinateX = event.pageX - 910;
-    let coordinateY = event.pageY - 177;
+    let coordinateY = event.pageY - 172;
     let xHidden = document.forms[0].elements[1].value = coordinateX;
     let yHidden = document.forms[0].elements[2].value = coordinateY;
 
